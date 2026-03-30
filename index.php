72085b7145d33cc6cb22c057c831d580e9d2727d
ThirdyNeko <ireneo.catequista@wvsu.edu.ph>
2026-03-30 11:55:21 +0800

    feat(access): protect main entry points with login requirement

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ session_start();
 $current_page = basename($_SERVER['PHP_SELF']);
 
 include 'config/db.php';
+include 'auth/require_login.php';
 include 'partials/header.php';
 include 'partials/sidebar.php';
 
